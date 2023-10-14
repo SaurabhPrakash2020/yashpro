35cf73552df14f18a47afdd40f3ff90b7f05075b
SaurabhPrakash2020 <112266991+SaurabhPrakash2020@users.noreply.github.com>
2023-10-14 12:35:52 +0530

    Update settings.py

--- sep/settings.py
+++ sep/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['16.171.168.247']
+ALLOWED_HOSTS = ['16.171.168.247:8000']
 
 
 # Application definition
